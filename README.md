4848af25506f897c1f9de23e0963888999fb930f
sophie_mdl <sophie.michaudel@gmail.com>
2017-05-07 15:43:47 +0200

    Ajout responsive et commentaires

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Snake
 Snake - Javacript
+
+Création d'un jeu de snake simple 
--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-var gameWidth = 1200,
-    gameHeight = 600;
-
-var userSpeed = 5;
-var snakeSlice;
 var pause = false;
-var head;
-var appleSize = snakeSize = 15;
-
-var gameArea = document.querySelector('#game');
-var tail;
-
 var collisionApple = false;
 
+var snakeSlice;
+var tail;
+var head;
+
+var appleSize = snakeSize = 15;
+
+var userSpeed;
+
+var gameArea = document.querySelector('#game');
+
 var game = {
-    width: gameArea.offsetWidth,
+    //La taille du terrain étant responsive, la taille est calculée dynamiquement à chaque lancement
+    // 8 = prise en compte de la bordure
+    width: gameArea.offsetWidth - 8,
     height: gameArea.offsetHeight,
-    x: gameArea.offsetLeft,
-    y: gameArea.offsetTop
 }
 
 var snake = {
@@ -44,9 +43,11 @@ var score = {
 
 function snakeBody() {
     for (var i = 0; i < snake.length; i++) {
+        //création d'un tableau contenant toutes les parties du serpent, avec pour chaque partie sa position x y.
         snake.body.push({ x: snakeSize * i, y: snake.y, snakeSlice });
     }
     for (var u = 0; u < snake.length; u++) {
+        //Pour chaque élément du tableau, une div de la classe snake est ajoutée à la page
         snake.body[u].snakeSlice = document.createElement("div");
         snake.body[u].snakeSlice.setAttribute('class', 'snake');
         gameArea.appendChild(snake.body[u].snakeSlice);
@@ -54,6 +55,7 @@ function snakeBody() {
 }
 
 function snakeDirection(e) {
+    //La direction change en fonction de la touche appuyée. Et espace = pause
     if (e.which === 38) { snake.direction = 'top'; }
     if (e.which === 40) { snake.direction = 'down'; }
     if (e.which === 39) { snake.direction = 'right'; }
@@ -65,19 +67,24 @@ function snakeDirection(e) {
 }
 
 function moveSnake() {
+    //head prend les valeurs de la dernière div placée sur le jeu
     head = snake.body[snake.length - 1];
-    console.log(snake.body)
+    //Les coordonnées du serpent sont déterminées par la position de head
     snake.x = head.x;
     snake.y = head.y;
+    //Le serpent avance d'une case (taille d'une 'tranche') en fonction de sa direction 
     if (snake.direction == 'top') { snake.y -= snake.size; }
     if (snake.direction == 'down') { snake.y += snake.size; }
     if (snake.direction == 'left') { snake.x -= snake.size; }
     if (snake.direction == 'right') { snake.x += snake.size; }
+    //S'il ne rencontre pas de pomme
     if (!collisionApple) {
+        //on retire la queue du seprent du tableau et on lui donne les coordonnées de la tête
         tail = snake.body.shift();
         tail.x = snake.x;
         tail.y = snake.y;
     } else {
+        //S'il rencontre une pomme, on crée un nouvel élément de tableau et on place une nouvelle div sur le terrain
         tail = { x: snake.x, y: snake.y, snakeSlice };
         tail.snakeSlice = document.createElement("div");
         tail.snakeSlice.setAttribute('class', 'snake');
@@ -85,40 +92,51 @@ function moveSnake() {
         snake.length += 1;
         collisionApple = false;
     }
+    //On ajoute la queue avec ses nouvelles coordonnées à la fin du tableau
     snake.body.push(tail);
 }
 
 function checkCollisions() {
+    //Si la tête du serpent rencontre une pomme
     if (((snake.x + snake.size) > apple.x) && //Droit
         (snake.x <= (apple.x + apple.size)) && //Gauche
         ((snake.y + snake.size) >= apple.y) && //Haut
         (snake.y <= (apple.y + apple.size))) { //bas
         resetApple();
         collisionApple = true;
-    }
-    if (snake.x < 0 || (snake.x + snake.size) > game.width || snake.y < 0 || (snake.y + snake.size) > game.height) {
+    } else if (snake.x <= 8 ||
+        (snake.x + snake.size) >= game.width ||
+        snake.y <= 8 ||
+        (snake.y + snake.size) >= game.height) {
+        //Si la tête du serpent sort du terrain
         pause = true;
     }
+
 }
 
 function resetApple() {
-    //Position aléatoire selon une grille correspondant à la taille du serpent
-    apple.x = Math.floor(Math.random() * ((game.width - apple.size) / 5)) * 5;
-    apple.y = Math.floor(Math.random() * ((game.height - apple.size) / 5)) * 5;
+    //Position aléatoire de la pomme dans le terrain
+    apple.x = Math.floor(Math.random() * game.width);
+    apple.y = Math.floor(Math.random() * game.height);
     apple.elem.style.top = apple.y + 'px';
     apple.elem.style.left = apple.x + 'px';
 }
 
 function init() {
+    //Initialisation du jeu :
+    //Placement de la pomme et création du serpent
+    //Mise en place d'une boucle toutes les 50 ms (dépendra de la vitesse de départ)
+    //Mise en place d'un écouteur de clavier
     resetApple();
-    setInterval(loop, 50);
     snakeBody();
+    setInterval(loop, 50);
     window.addEventListener('keydown', function(e) {
         snakeDirection(e);
     });
 }
 
 function render() {
+    //Changement de position de chaque élément du tableau serpent et calcul du score
     for (var i = 0; i < snake.length; i++) {
         snake.body[i].snakeSlice.style.top = snake.body[i].y + 'px';
         snake.body[i].snakeSlice.style.left = snake.body[i].x + 'px';
@@ -126,7 +144,8 @@ function render() {
 }
 
 function loop() {
-    if (pause) {
+    //Si le jeu n'est pas en pause, boucle qui fait bouger le serpent, vérifie les collisions et affiche le rendu à l'écran
+    if (!pause) {
         moveSnake();
         checkCollisions();
         render();
